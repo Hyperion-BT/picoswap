0177c8b1ff6184d54d47c798fc12c7905e2cbcea
Christian Schmitz <cschmitz398@gmail.com>
2022-10-09 22:45:17 -0500

    changed helios library url

--- helios.js
+++ helios.js
@@ -1,2 +1,2 @@
 // don't need @deno-types because everything is in JSDocs
-export { Address, Assets, ConstrData, Datum, Hash, InlineDatum, IntData, MintingPolicyHash, NetworkParams, Program, PubKeyHash, Value, UTxO, Tx, TxOutput, TxWitnesses, Signature, highlight, bytesToHex, hexToBytes, UplcData, UplcProgram } from "https://hyperion-bt.github.io/Helios/helios.js";
+export { Address, Assets, ConstrData, Datum, Hash, InlineDatum, IntData, MintingPolicyHash, NetworkParams, Program, PubKeyHash, Value, UTxO, Tx, TxOutput, TxWitnesses, Signature, highlight, bytesToHex, hexToBytes, UplcData, UplcProgram } from "https://www.hyperion-bt.org/Helios/helios.js";
